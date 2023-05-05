b4f1939585c666522ee415e81ab594bace281541
Emily Iannizzi <eiannizzi18@gmail.com>
2023-05-05 15:45:37 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+    if (button) {
+      button.addEventListener('click', function() {
+        console.log('Button clicked!');
+      });
+    } else {
+      console.error('Could not find button element with ID "button"');
+    }
+  }
+
+  
+  
